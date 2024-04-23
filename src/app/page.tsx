1f4b830524f641b7d902607ac652e0245fc45961
Thomas Wilson <thomas.wilson@scale.com>
2024-04-23 15:45:21 -0400

    fix: add redirect to avoid filler landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default async function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center dark bg-background text-foreground">
-      <div>Hello World</div>
-    </main>
-  );
+  redirect('/mail');
 }
